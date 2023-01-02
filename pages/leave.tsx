8a32ba11c937b2771fa83eb827df08c905c11a1d
Shashank_yadav <yadavshashank0907@gmail.com>
2023-01-02 14:55:30 +0530

    created left sidebar of leave-page

--- pages/leave.tsx
+++ pages/leave.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './components/header'
+import LeaveLeftSide from './components/leaveLeftSide'
 
 function leave() {
-  return (
-    <div>leave</div>
-  )
+  const [selected,setSelected]=useState(" ");
+  return <>
+    <Header />
+    <LeaveLeftSide  selected={selected} setSelected={setSelected}/>
+    </>
 }
 
 export default leave
--- pages/meeting.tsx
+++ pages/meeting.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from './components/header'
-import calender from './components/calender'
+//import calender from './components/calender'
 
 function meeting() {
   return (
     <div>
       <Header />
-      <calender />
+      {/* <calender /> */}
     </div>
     
   )
