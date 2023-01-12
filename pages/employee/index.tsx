d968088275964819b5e9f88a58994545affaad3f
Anshu Mishra <anshumishra1258@gmail.com>
2023-01-12 21:48:23 +0530

    resolve conflict

--- pages/employee/index.tsx
+++ pages/employee/index.tsx
@@ -9,12 +9,7 @@ function employee() {
     router.push("/AddEmployee")
   }
   return (
-    <div>
-      <div id="heading">
-        <div
-          id="employee-head"
-          className="flex space-x-20 justify-between max-w-1280px mx-auto py-2 px-4 bg-red-50 border-solid border-y-2 border-black-500"
-        >
+    <div id="employee-head" className="flex space-x-20 justify-between max-w-1280px mx-auto py-2 px-4 bg-red-50 border-solid border-y-2 border-black-500">
           <div>
             <h3 className="font-bold my-3">Employee</h3>
           </div>
@@ -23,14 +18,13 @@ function employee() {
 
             <FilterIcon />
             <GridIcon />
-            <button className="border-2 px-4 py-0.4 hover:border-t-4 rounded-2xl">
+            {/* <button className="border-2 px-4 py-0.4 hover:border-t-4 rounded-2xl"> */}
             <button className="border-2 px-4 py-0.4 hover:border-t-4 rounded-2xl" onClick={addEmployeeButton}>
               Add
             </button>
           </div>
-        </div>
-      </div>
-    </div>
+   </div>
+
   );
 }
 
